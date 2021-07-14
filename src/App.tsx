8c480af90ac2775e98358db16c10911c1c671cec
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-07-14 17:36:41 +0200

    feat: Fintionc Props

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import "./App.css";
 import Todo from "./models/todo";
 
 import Todos from "./components/Todos";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
 
+  const addTodoHandler = (todoText: string) => {
+
+  };
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos} />
     </div>
   );
